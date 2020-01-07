2c9263e9a92339d5b38381d2435d5ea6a916cc79
Neel Patel <321neel@gmail.com>
2020-01-06 21:17:14 -0500

    Added transitions + event listeners.

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const panels = document.querySelectorAll('.panel');
 function toggleOpen() {
     console.log('heyy');
     this.classList.toggle('open');
+
+}
+
+function toggleActive(){
+    if(e.propertyName === 'flex-grow')
+    // this.classList.toggle('open-active');
 }
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive))
